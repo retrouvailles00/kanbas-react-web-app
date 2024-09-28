46a44984d7cc6a96770ef9fe8f9123988a2b2e63
retrouvailles00 <retrouvailles0@hotmail.com>
2024-09-27 23:30:25 -0700

    a2 account, home, modules, people

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -3,19 +3,44 @@ export default function Profile() {
     return (
         <div id="wd-profile-screen">
             <h3>Profile</h3>
-            <input id="wd-username" value="alice" placeholder="username" /><br/>
-            <input id="wd-password" value="123" placeholder="password"
-                   type="password" /><br/>
-            <input id="wd-firstname" value="Alice" placeholder="First Name" /><br/>
-            <input id="wd-lastname" value="Wonderland" placeholder="Last Name" /><br/>
-            <input id="wd-dob" value="2000-01-01" type="date" /><br/>
-            <input id="wd-email" value="alice@wonderland" type="email" /><br/>
-            <select id="wd-role">
+            <input id="wd-username"
+                   placeholder="username"
+                   value="alice"
+                   className="form-control mb-2"/>
+            <input id="wd-password"
+                   placeholder="password"
+                   value="123"
+                   className="form-control mb-2"/>
+            <input id="wd-firstname"
+                   placeholder="First Name"
+                   value="Alice"
+                   className="form-control mb-2"/>
+            <input id="wd-lastname"
+                   placeholder="Last Name"
+                   value="Wonderland"
+                   className="form-control mb-2"/>
+
+            <input id="wd-lastname"
+                   placeholder="Last Name"
+                   value="Wonderland"
+                   className="form-control mb-2"/>
+
+            <input id="wd-dob"
+                   type="date"
+                   value="2000-01-01"
+                   className="form-control mb-2"/>
+            <input id="wd-email"
+                   type="email"
+                   value="alice@wonderland"
+                   className="form-control mb-2"/>
+
+            <select id="wd-role" className="form-control mb-2">
                 <option value="USER">User</option>
                 <option value="ADMIN">Admin</option>
                 <option value="FACULTY">Faculty</option>
                 <option value="STUDENT">Student</option>
             </select><br/>
-            <Link to="/Kanbas/Account/Signin" >Sign out</Link>
+            <Link to="/Kanbas/Account/Signin" className="btn btn-danger w-100">Sign out</Link>
         </div>
-    );}
+    );
+}
--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,25 +1,51 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import {LiaBullhornSolid, LiaFileImportSolid} from "react-icons/lia";
+import {IoIosNotifications, IoMdHome} from "react-icons/io";
+import {RiBarChart2Fill} from "react-icons/ri";
 export default function CourseStatus() {
     return (
-        <div id="wd-course-status">
+        <div id="wd-course-status-wrapper" className={"p-3"}>
+        <div id="wd-course-status" style={{width: "300px"}}>
             <h2>Course Status</h2>
-            <p>
-                <button>Unpublish</button>
-                <button>Publish</button>
-            </p>
-            <button>Import Existing Content</button>
+            <div className="d-flex">
+                <div className="w-50 pe-1">
+                    <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+                        <MdDoNotDisturbAlt className="me-2 fs-5"/> Unpublish
+                    </button>
+                </div>
+                <div className="w-50">
+                    <button className="btn btn-lg btn-success w-100">
+                        <FaCheckCircle className="me-2 fs-5"/> Publish
+                    </button>
+                </div>
+            </div>
             <br/>
-            <button>Import from Commons</button>
-            <br/>
-            <button>Choose Home Page</button>
-            <br/>
-            <button>View Course Stream</button>
-            <br/>
-            <button>New Announcement</button>
-            <br/>
-            <button>New Analytics</button>
-            <br/>
-            <button>View Course Notifications</button>
-            <br/>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <BiImport className="me-2 fs-5"/> Import Existing Content
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <LiaFileImportSolid className="me-2 fs-5"/> Import from Commons
+            </button>
+
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <IoMdHome className="me-2 fs-5"/> Choose Home Page
+            </button>
+
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <RiBarChart2Fill className="me-2 fs-5"/> View Course Stream
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <LiaBullhornSolid className="me-2 fs-5"/> New Announcement
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <RiBarChart2Fill className="me-2 fs-5"/> New Analytics
+            </button>
+            <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+                <IoIosNotifications className="me-2 fs-5"/> View Course Notifications
+            </button>
         </div>
+            </div>
     );
 }
