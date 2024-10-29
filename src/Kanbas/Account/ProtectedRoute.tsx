83428b7359e81287022a88a139abb709a0d7578d
retrouvailles00 <liu.rui4@northeastern.edu>
2024-10-29 00:46:52 -0700

    fix bug

--- src/Kanbas/Account/ProtectedRoute.tsx
+++ src/Kanbas/Account/ProtectedRoute.tsx
@@ -3,20 +3,25 @@ import { Navigate } from "react-router-dom";
 import {useParams} from "react-router";
 import * as db from "../Database";
 export default function ProtectedRoute({ children }: { children: any }) {
-    const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const { cid } = useParams();
+    const {currentUser} = useSelector((state: any) => state.accountReducer);
+    const {cid} = useParams();
     const enrollments = db.enrollments;
 
-    const isEnrolled = enrollments.some(
-        (enrollment) =>
-            enrollment.user === currentUser._id &&
-            enrollment.course === cid
-    );
     if (!currentUser) {
-        return <Navigate to="/Kanbas/Account/Signin" />;
-    }
-    else if (!cid || isEnrolled) {
-        return children;
+        return <Navigate to="/Kanbas/Account/Signin"/>;
     } else {
-        return <Navigate to="/Kanbas/Dashboard" />;
-    }}
+        if (!cid ) {
+            return children;
+        }
+        const isEnrolled = enrollments.some(
+            (enrollment) =>
+                enrollment.user === currentUser._id &&
+                enrollment.course === cid
+        );
+        if (!isEnrolled) {
+            return <Navigate to="/Kanbas/Dashboard"/>;
+        } else {
+            return children;
+        }
+    }
+}
